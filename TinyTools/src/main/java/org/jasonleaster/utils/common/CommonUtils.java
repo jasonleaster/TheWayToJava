0533ba9c4d2a2cdf06749cdc35c8e725d9bde0fd
jasonleaster <jasonleaster@gmail.com>
2017-07-01 09:41:42 +0800

    update commonutils

--- TinyTools/src/main/java/org/jasonleaster/utils/common/CommonUtils.java
+++ TinyTools/src/main/java/org/jasonleaster/utils/common/CommonUtils.java
@@ -1,6 +1,8 @@
 package org.jasonleaster.utils.common;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
 /**
@@ -13,6 +15,25 @@ public class CommonUtils {
 
     public static <K,V> V getMapValueWithSupplier(Map<K,V> map, K key, Supplier<V> supplier){
         return map.computeIfAbsent(key, k -> supplier.get());
+    }
+
+    public static <K,V> Map<K,V> mergeMapVal(Map<K, V> baseMap, Map<K, V> otherMap, BiFunction<V, V, V> function){
+        Map<K,V> result = new HashMap<>(baseMap);
+
+//        otherMap.forEach((key, val) ->
+//            {
+//                baseMap.computeIfAbsent(key,
+//                    k -> result.put(k, val));
+//
+//                baseMap.computeIfPresent(key,
+//                    (k, v) -> result.put(k,
+//                        function.apply(v, val)));
+//            }
+//        );
+
+        otherMap.forEach((key, val) -> result.merge(key, val, function));
+
+        return result;
     }
 
 }
